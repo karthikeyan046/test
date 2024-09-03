fd6ac027de3a6ba8e9e01fda199b2a67fdeda95f
karthikeyan046 <karthikeyan046@yahoo.co.in>
2024-09-03 11:20:07 +0000

    Update print_notebook.py

--- GCP/print_notebook.py
+++ GCP/print_notebook.py
@@ -1,1 +1,19 @@
+# Read_json.py
+import json
+
+# Assume we're reading a JSON file and storing it in a variable
+with open("config.json", "r") as file:
+    data = json.load(file)
+
+# Define a global variable to store the result
+global config_data
+config_data = data
+
+
+
+# Execute the script
+#%run "C:\Users\karthi\Desktop\Testnotebook\Read_json.ipynb"
+
+# Now you can access config_data directly
+#print(config_data)
 
