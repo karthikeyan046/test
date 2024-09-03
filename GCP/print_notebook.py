9c09ae70c966b851068caf466b260a91853c2ad0
karthikeyan046 <karthikeyan046@yahoo.co.in>
2024-09-03 11:48:04 +0000

    Update print_notebook.py

--- GCP/print_notebook.py
+++ GCP/print_notebook.py
@@ -17,3 +17,28 @@ config_data = data
 # Now you can access config_data directly
 #print(config_data)
 
+'''
+import json
+
+def read_jsonValues():
+    with open("config.json", "r") as file:
+        data = json.load(file)
+        #print(data)
+    return data
+
+
+# Run the other notebook
+%run ./read_j.ipynb
+
+# Now you can use the variables or functions defined in the other notebook
+res=read_jsonValues()
+print(res['var1'])
+
+
+
+
+''''
+
+
+
+
