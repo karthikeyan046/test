4a3fcb507beadd0e3de1b577ecc868da8aa88c97
karthikeyan046 <karthikeyan046@yahoo.co.in>
2024-09-30 13:25:24 +0530

    Update print_file_name.py

--- print_file_name.py
+++ print_file_name.py
@@ -1,1 +1,19 @@
+import os
+
+# Folder path
+folder_path = "C:\\Users\\ksubraman1\\Desktop\\uc"
+
+# Output text file path
+output_file_path = "C:\\Users\\ksubraman1\\Desktop\\uc\\file_list.txt"
+
+# Get all files in the folder
+files = os.listdir(folder_path)
+
+# Write filenames to the text file
+with open(output_file_path, 'w') as f:
+    for file in files:
+        # Only write files, not directories
+        if os.path.isfile(os.path.join(folder_path, file)):
+            f.write(file + '\n')
+            print(file)
 
